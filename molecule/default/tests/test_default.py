d5b86af3a2c2bac31e9ba76ef47d876de8ec50a7
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2024-08-19 15:32:35 -0400

    Specify the full path for pip3
    Ensure that we are using the system Python environment. This aligns
    with what we do in other roles, the needs of the cisagov/cyhy-runner
    package, and the configuration in this role. The molecule testing
    configuration is also updated to ensure it uses the system Python
    environment.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -36,7 +36,7 @@ def test_packages(host):
 @pytest.mark.parametrize("pkg", ["cyhy-runner"])
 def test_pip_packages(host, pkg):
     """Test that the pip packages were installed."""
-    assert pkg in host.pip.get_packages(pip_path="pip3")
+    assert pkg in host.pip.get_packages(pip_path="/usr/bin/pip3")
 
 
 @pytest.mark.parametrize(
